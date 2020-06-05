0331bfc5a4bbdc0d6dc3d82b20ef9e41014f3125
solomonfy <fantahunsolomon@gmail.com>
2020-06-05 00:53:17 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        # @@all << self
+        save
+    end
+
+    def self.all
+       @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        puts @@all.map {|d| d.name}
+        # puts self.name
+    end
+    # binding.pry
+
+    def save
+        @@all << self
+    end
+
+end
+
