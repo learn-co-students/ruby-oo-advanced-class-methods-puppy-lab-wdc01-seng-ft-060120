e5f9fe20b254cdecd99a10ea874453200873e044
Tien Phan <tienphan1989@gmail.com>
2020-06-04 21:20:39 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog
+
+    @@all = Array.new
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def name
+        @name
+    end
+
+    def self.print_all
+        @@all.each { |dog| puts dog.name}
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+end
